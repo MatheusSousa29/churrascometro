f874df263e831bfe959d8c02229f5d12fb489cd5
Matheus de Sousa <matheus.dev29@gmail.com>
2022-09-19 19:23:05 -0300

    add

---
+++ script.js
@@ -0,0 +1,49 @@
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+  console.log("Calculando...")
+  
+  let adultos = inputAdultos.value;
+  let criancas = inputCriancas.value;
+  let duracao = inputDuracao.value;
+
+  
+  let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+  let qdtTotalCerveja = cervejaPP(duracao) * adultos;
+  let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+  
+  resultado.innerHTML = `<p><b>${qdtTotalCarne / 1000} Kg de Carne</b></p>`
+  resultado.innerHTML += `<p><b>${Math.ceil(qdtTotalCerveja / 355)} Latas de cerveja</b></p>`
+  resultado.innerHTML += `<p><b>${Math.ceil(qdtTotalBebidas / 2000)} Pet's 2 Litros de Refrigerante</b></p>`
+ 
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
+
+
